500bf3d456b60a6dc07180532323c754c043b4d1
Ellaine Gonzales <ellainegonzales003@gmail.com>
2025-11-24 23:51:44 +0800

    Send data to postgresql database

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const app = express();
 const port = 3000;
 
 const date = new Date();
-const month = date.getMonth();
+const month = date.getMonth() + 1;
 const day = date.getDate();
 const year = date.getFullYear
 const firstDay = new Date(year, month, 1);
@@ -25,11 +25,23 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) =>{
-    res.render("main.ejs", {month: month +1, day: day});
+    res.render("main.ejs", {month: month, day: day});
 });
 
 app.get("/addEvent", (req, res) => {
-    res.render("addEvent.ejs", {month: month + 1, day: day});
+    res.render("addEvent.ejs", {month: month, day: day});
+});
+
+app.post("/submitEvent", async(req, res)=>{
+    const title = req.body.title;
+    const occasion = req.body.categoryType;
+    const description = req.body.descriptionArea;
+    try {
+        await db.query("INSERT INTO occasions (title, categoryType, description, created_at) VALUES ($1, $2, $3, $4);", [title, occasion, description, date]);
+        res.redirect("/");
+    } catch (err) {
+        console.log("Error occured: ", err);
+    }
 });
 
 app.listen(port,()=>{
