d6c98ee76f0609fea94b5758de91c8481a1255ee
Ellaine Gonzales <ellainegonzales003@gmail.com>
2025-11-25 23:18:28 +0800

    Add styling to the horizontal scroll bar

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import bodyParser from "body-parser";
 import pg from "pg";
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "projects",
-    password: "ekUbalu0902!",
-    port: 5432
+  user: "postgres",
+  host: "localhost",
+  database: "projects",
+  password: "ekUbalu0902!",
+  port: 5432,
 });
 
 db.connect();
@@ -18,33 +18,53 @@ const port = 3000;
 const date = new Date();
 const month = date.getMonth() + 1;
 const day = date.getDate();
-const year = date.getFullYear
-const firstDay = new Date(year, month, 1);
+const year = date.getFullYear();
+const lastDay = new Date(year, month, 0).getDate(); //last day of the current month
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) =>{
-    res.render("main.ejs", {month: month, day: day});
+app.get("/", (req, res) => {
+  res.render("main.ejs", {
+    month: month,
+    day: day,
+    year: year,
+    lastDay: lastDay,
+    selectedDate: null,
+  });
 });
 
 app.get("/addEvent", (req, res) => {
-    res.render("addEvent.ejs", {month: month, day: day});
+  let selectedDate;
+
+  if (req.query.date) {
+    selectedDate = new Date(req.query.date);
+  } else {
+    selectedDate = new Date();
+  }
+  const year = selectedDate.getFullYear();
+  const month = selectedDate.getMonth() + 1;
+  const day = selectedDate.getDate();
+  res.render("addEvent.ejs", { month: month, day: day, year:year });
 });
 
-app.post("/submitEvent", async(req, res)=>{
-    const title = req.body.title;
-    const occasion = req.body.categoryType;
-    const description = req.body.descriptionArea;
-    try {
-        await db.query("INSERT INTO occasions (title, categoryType, description, created_at) VALUES ($1, $2, $3, $4);", [title, occasion, description, date]);
-        res.redirect("/");
-    } catch (err) {
-        console.log("Error occured: ", err);
-    }
+app.post("/submitEvent", async (req, res) => {
+  const title = req.body.title;
+  const occasion = req.body.categoryType;
+  const description = req.body.descriptionArea;
+  const selectDate = req.body.selectedDate;
+  try {
+    await db.query(
+      "INSERT INTO occasions (title, date, categoryType, description, created_at) VALUES ($1, $2, $3, $4, $5);",
+      [title, selectDate, occasion, description, date]
+    );
+    res.redirect("/");
+  } catch (err) {
+    console.log("Error occured: ", err);
+  }
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running port ${port}`);
-    console.log(month + 1);
+app.listen(port, () => {
+  console.log(`Server is running port ${port}`);
+  console.log(month + 1);
 });
