98e43e635f503561537777c322944ae1fb715190
Ellaine Gonzales <ellainegonzales003@gmail.com>
2025-12-10 09:18:55 +0800

    Add .env

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
+
+env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "projects",
-  password: "ekUbalu0902!",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 db.connect();
